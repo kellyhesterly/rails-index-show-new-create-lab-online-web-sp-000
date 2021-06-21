f957906930d97049e15cc97f5b871ca88c46d4b5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 19:02:09 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,6 +8,6 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
-    redirect_to coupon_path(@post)
+    redirect_to coupon_path(@coupon)
   end
 end

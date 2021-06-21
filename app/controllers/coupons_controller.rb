2d9fc1308195d1e9850428d248525c1ff111f138
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 18:47:50 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,7 +1,9 @@
 class CouponsController < ApplicationController
 
   def create
-    @coupon = Coupon.create(params)
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
     redirect_to coupon_path(@post)
   end
 end

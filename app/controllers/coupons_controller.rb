5413e4d5109d41240804b9e2b0703d3254782b0b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 19:20:37 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,16 +5,16 @@ class CouponsController < ApplicationController
   end
 
   def show
-     @coupon = Coupon.find(params[:id])
-   end
+    @coupon = Coupon.find(params[:id])
+  end
 
   def new
   end
 
   def create
     @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon_code]
-    @coupon.store = params[:store]
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
   end
